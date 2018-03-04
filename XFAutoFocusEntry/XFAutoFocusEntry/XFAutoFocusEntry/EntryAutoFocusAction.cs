f38eba775debae48483344b748ea8660355cf794
xamarindevelopervietnam <xamarindevelopervietnam@outlook.com>
2018-03-04 09:11:20 +0700

    first commit

---
+++ XFAutoFocusEntry/XFAutoFocusEntry/XFAutoFocusEntry/EntryAutoFocusAction.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Behaviors.Base;
+using Behaviors.Interface;
+using Xamarin.Forms;
+
+namespace XFAutoFocusEntry
+{
+    /*how to use
+     <controls:Entry.Behaviors>
+                    <behaviors:EventHandlerBehavior EventName="ReturnRequest">
+                        <behaviors:InvokeCommandAction Command="{Binding NextStepCommand}" CommandParameter="-1" />
+                    </behaviors:EventHandlerBehavior>
+                </controls:Entry.Behaviors>
+
+        <controls:Entry.Behaviors>
+                    <behaviors:EventHandlerBehavior EventName="ReturnRequest">
+                        <controls:EntryAutoFocusAction TargetObject="{x:Reference EntryUserPhone}" />
+                    </behaviors:EventHandlerBehavior>
+                </controls:Entry.Behaviors>
+         
+         */
+    public class EntryAutoFocusAction : AnimationBase, IAction
+    {
+        public Task<bool> Execute(object sender, object parameter)
+        {
+            Entry element;
+            if (TargetObject != null)
+            {
+                element = TargetObject as Entry;
+            }
+            else
+            {
+                element = sender as Entry;
+            }
+
+            if (element == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            //auto focus
+            element.Focus();
+
+            return Task.FromResult(true);
+        }
+    }
+}
